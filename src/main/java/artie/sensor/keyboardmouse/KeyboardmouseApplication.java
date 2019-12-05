f57a91e1e69713d8f766ad803fb42a5451d6957f
Luis Eduardo Imbernón <leimbernon@ai-mind.com>
2019-12-05 12:37:57 +0100

    Modified the packaging and the properties file.

--- src/main/java/artie/sensor/keyboardmouse/KeyboardmouseApplication.java
+++ src/main/java/artie/sensor/keyboardmouse/KeyboardmouseApplication.java
@@ -9,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class KeyboardmouseApplication {
 	
 	public static void main(String[] args) {
+		System.setProperty("spring.config.name", "keyboardmouse");
 		SpringApplication.run(KeyboardmouseApplication.class, args);
 	}
 
